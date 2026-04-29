4f9dbf2041b952c3f0ca70473bb941816603ccb0
Sujit Kumar Sethi <143935792+sujitmanw@users.noreply.github.com>
2026-04-29 11:48:43 +0530

    Update contentDetails.js

--- contentDetails.js
+++ contentDetails.js
@@ -1,106 +1,147 @@
-console.clear();
+console.clear()
 
-let id = location.search.split('?')[1];
-console.log(id);
+let id = location.search.split('?')[1]
+console.log(id)
 
-// ✅ Safe badge update - wait for header to load
-function updateBadge() {
-  const badge = document.getElementById("badge");
-  if (badge && document.cookie.indexOf(',counter=') >= 0) {
-    let counter = document.cookie.split(',')[1].split('=')[1];
-    badge.innerHTML = counter;
-  }
+if(document.cookie.indexOf(',counter=')>=0)
+{
+    let counter = document.cookie.split(',')[1].split('=')[1]
+    document.getElementById("badge").innerHTML = counter
 }
 
-// Wait for header fetch to complete
-setTimeout(updateBadge, 600);
+function dynamicContentDetails(ob)
+{
+    let mainContainer = document.createElement('div')
+    mainContainer.id = 'containerD'
+    document.getElementById('containerProduct').appendChild(mainContainer);
 
-function dynamicContentDetails(ob) {
-  let mainContainer = document.createElement('div');
-  mainContainer.id = 'containerD';
-  document.getElementById('containerProduct').appendChild(mainContainer);
+    let imageSectionDiv = document.createElement('div')
+    imageSectionDiv.id = 'imageSection'
 
-  let imageSectionDiv = document.createElement('div');
-  imageSectionDiv.id = 'imageSection';
+    let imgTag = document.createElement('img')
+     imgTag.id = 'imgDetails'
+     //imgTag.id = ob.photos
+     imgTag.src = ob.preview
 
-  let imgTag = document.createElement('img');
-  imgTag.id = 'imgDetails';
-  imgTag.src = ob.preview;
-  imageSectionDiv.appendChild(imgTag);
+    imageSectionDiv.appendChild(imgTag)
 
-  let productDetailsDiv = document.createElement('div');
-  productDetailsDiv.id = 'productDetails';
+    let productDetailsDiv = document.createElement('div')
+    productDetailsDiv.id = 'productDetails'
 
-  let h1 = document.createElement('h1');
-  h1.appendChild(document.createTextNode(ob.name));
+    // console.log(productDetailsDiv);
 
-  let h4 = document.createElement('h4');
-  h4.appendChild(document.createTextNode(ob.brand));
+    let h1 = document.createElement('h1')
+    let h1Text = document.createTextNode(ob.name)
+    h1.appendChild(h1Text)
 
-  let detailsDiv = document.createElement('div');
-  detailsDiv.id = 'details';
+    let h4 = document.createElement('h4')
+    let h4Text = document.createTextNode(ob.brand)
+    h4.appendChild(h4Text)
+    console.log(h4);
 
-  let h3Price = document.createElement('h3');
-  h3Price.appendChild(document.createTextNode('Rs ' + ob.price));
+    let detailsDiv = document.createElement('div')
+    detailsDiv.id = 'details'
 
-  let h3Desc = document.createElement('h3');
-  h3Desc.appendChild(document.createTextNode('Description'));
+    let h3DetailsDiv = document.createElement('h3')
+    let h3DetailsText = document.createTextNode('Rs ' + ob.price)
+    h3DetailsDiv.appendChild(h3DetailsText)
 
-  let para = document.createElement('p');
-  para.appendChild(document.createTextNode(ob.description));
+    let h3 = document.createElement('h3')
+    let h3Text = document.createTextNode('Description')
+    h3.appendChild(h3Text)
 
-  let productPreviewDiv = document.createElement('div');
-  productPreviewDiv.id = 'productPreview';
+    let para = document.createElement('p')
+    let paraText = document.createTextNode(ob.description)
+    para.appendChild(paraText)
 
-  let h3Preview = document.createElement('h3');
-  h3Preview.appendChild(document.createTextNode('Product Preview'));
-  productPreviewDiv.appendChild(h3Preview);
+    let productPreviewDiv = document.createElement('div')
+    productPreviewDiv.id = 'productPreview'
 
-  for (let i = 0; i < ob.photos.length; i++) {
-    let previewImg = document.createElement('img');
-    previewImg.id = 'previewImg';
-    previewImg.src = ob.photos[i];
-    previewImg.onclick = function() {
-      document.getElementById("imgDetails").src = this.src;
-    };
-    productPreviewDiv.appendChild(previewImg);
-  }
+    let h3ProductPreviewDiv = document.createElement('h3')
+    let h3ProductPreviewText = document.createTextNode('Product Preview')
+    h3ProductPreviewDiv.appendChild(h3ProductPreviewText)
+    productPreviewDiv.appendChild(h3ProductPreviewDiv)
 
-  let buttonDiv = document.createElement('div');
-  buttonDiv.id = 'button';
-
-  let buttonTag = document.createElement('button');
-  let buttonText = document.createTextNode('Add to Cart');
-
-  buttonTag.onclick = function() {
-    let order = id + " ";
-    let counter = 1;
-    if (document.cookie.indexOf(',counter=') >= 0) {
-      order = id + " " + document.cookie.split(',')[0].split('=')[1];
-      counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1;
+    let i;
+    for(i=0; i<ob.photos.length; i++)
+    {
+        let imgTagProductPreviewDiv = document.createElement('img')
+        imgTagProductPreviewDiv.id = 'previewImg'
+        imgTagProductPreviewDiv.src = ob.photos[i]
+        imgTagProductPreviewDiv.onclick = function(event)
+        {
+            console.log("clicked" + this.src)
+            imgTag.src = ob.photos[i]
+            document.getElementById("imgDetails").src = this.src 
+            
+        }
+        productPreviewDiv.appendChild(imgTagProductPreviewDiv)
     }
-    document.cookie = "orderId=" + order + ",counter=" + counter;
-    const badge = document.getElementById("badge");
-    if (badge) badge.innerHTML = counter;
-    console.log(document.cookie);
-  };
 
-  buttonTag.appendChild(buttonText);
-  buttonDiv.appendChild(buttonTag);
+    let buttonDiv = document.createElement('div')
+    buttonDiv.id = 'button'
 
-  mainContainer.appendChild(imageSectionDiv);
-  mainContainer.appendChild(productDetailsDiv);
-  productDetailsDiv.appendChild(h1);
-  productDetailsDiv.appendChild(h4);
-  productDetailsDiv.appendChild(detailsDiv);
-  detailsDiv.appendChild(h3Price);
-  detailsDiv.appendChild(h3Desc);
-  detailsDiv.appendChild(para);
-  productDetailsDiv.appendChild(productPreviewDiv);
-  productDetailsDiv.appendChild(buttonDiv);
+    let buttonTag = document.createElement('button')
+    buttonDiv.appendChild(buttonTag)
 
-  return mainContainer;
+    buttonText = document.createTextNode('Add to Cart')
+    buttonTag.onclick  =   function()
+    {
+        let order = id+" "
+        let counter = 1
+        if(document.cookie.indexOf(',counter=')>=0)
+        {
+            order = id + " " + document.cookie.split(',')[0].split('=')[1]
+            counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1
+        }
+        document.cookie = "orderId=" + order + ",counter=" + counter
+        document.getElementById("badge").innerHTML = counter
+        console.log(document.cookie)
+    }
+    buttonTag.appendChild(buttonText)
+
+
+    console.log(mainContainer.appendChild(imageSectionDiv));
+    mainContainer.appendChild(imageSectionDiv)
+    mainContainer.appendChild(productDetailsDiv)
+    productDetailsDiv.appendChild(h1)
+    productDetailsDiv.appendChild(h4)
+    productDetailsDiv.appendChild(detailsDiv)
+    detailsDiv.appendChild(h3DetailsDiv)
+    detailsDiv.appendChild(h3)
+    detailsDiv.appendChild(para)
+    productDetailsDiv.appendChild(productPreviewDiv)
+    
+    
+    productDetailsDiv.appendChild(buttonDiv)
+
+
+    return mainContainer
 }
 
-// BACKEND CALL
-let httpRequest = new XMLHttpRequest
+
+
+// BACKEND CALLING
+
+let httpRequest = new XMLHttpRequest()
+{
+    httpRequest.onreadystatechange = function()
+    {
+        if(this.readyState === 4 && this.status == 200)
+        {
+            console.log('connected!!');
+            let contentDetails = JSON.parse(this.responseText)
+            {
+                console.log(contentDetails);
+                dynamicContentDetails(contentDetails)
+            }
+        }
+        else
+        {
+            console.log('not connected!');
+        }
+    }
+}
+
+httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/'+id, true)
+httpRequest.send()  
